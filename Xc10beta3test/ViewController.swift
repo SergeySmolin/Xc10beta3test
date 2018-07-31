7f5e030f1c1705977d4f0a28c87e7f514083bbbb
Sergey Smolin <soees84@gmail.com>
2018-07-31 19:16:55 +0500

    Initial Commit

---
+++ Xc10beta3test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xc10beta3test
+//
+//  Created by Sergey Smolin on 31/07/2018.
+//  Copyright © 2018 Sergey Smolin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
